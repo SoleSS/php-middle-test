766f782d30f2c9f1ad09b2011ee4a2105edac61b
root <root@debian-jessie.vagrantup.com>
2019-02-10 11:16:44 +0300

    + Nginx/OpenResty config

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,12 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'user' => [
+            // following line will restrict access to profile, recovery, registration and settings controllers from backend
+            //'as backend' => 'dektrium\user\filters\BackendFilter',
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
--- common/config/main.php
+++ common/config/main.php
@@ -10,4 +10,16 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            // you will configure your module inside this file
+            // or if need different configuration for frontend and backend you may
+            // configure in needed configs
+            'admins' => ['admin', ],
+            'adminPermission' => ['Administrator', ],
+        ],
+        'rbac' => 'dektrium\rbac\RbacWebModule',
+    ],
+
 ];
